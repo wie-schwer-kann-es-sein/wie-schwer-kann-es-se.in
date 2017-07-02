18523ebba672f155d28b8b741c0850e274ea6125
Frederic Darmstädter <frederic@darmstaedter.org>
2017-07-02 17:23:28 +0200

    📦 Podcast PageType & Anfang vom Podcast-Feed

---
+++ Packages/Sites/WieSchwerKannEsSein.Website/Classes/ViewHelpers/ItunesViewHelper.php
@@ -0,0 +1,10 @@
+<?php
+namespace WieSchwerKannEsSein\Website\ViewHelpers;
+
+use Neos\FluidAdaptor\Core\ViewHelper\AbstractTagBasedViewHelper;
+
+class ItunesViewHelper extends \Neos\FluidAdaptor\Core\ViewHelper\AbstractViewHelper {
+        public function render() {
+                return 'Hello World';
+        }
+}
